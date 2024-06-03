5c8115eff95632e620a81b4f0d5c8d893fbdbab3
Hrasek1000cz <69074536+Hrasek1000cz@users.noreply.github.com>
2024-06-03 17:36:29 +0200

    0.0.1

--- javascript.js
+++ javascript.js
@@ -8,105 +8,86 @@ let MAXNUM; //!!!Nesmí překročit číslo 12!!!!
 const zdrazeni = 0.05;
 //Hodnota i zde znamená, nakolik desetinných míst chceme ukázat finální kurzy, nastavuje se v url parametrech
 let i;
-//Překlady pro českou a anglickou verzi v div pod class „zeme“
-const translZem = {
-    "cz": "Země:",
-    "en": "Country:"
-};
-//Překlady pro českou a anglickou verzi v div pod class „mena“
-const translMen= {
-    "cz": "Měna:",
-    "en": "Currency:"
-};
-//Překlady pro českou a anglickou verzi v div pod class „kurz_prodej“
-const translKKo= {
-    "cz": "Koupíme za:",
-    "en": "We buy for"
-};
-//Překlady pro českou a anglickou verzi v div pod class „kurz_koupe“
-const translKPr= {
-    "cz": "Prodáme za:",
-    "en": "We sell for:"
-};
-//Překlady pro českou a anglickou verzi v div pod class „kurz_koupe“
-const actCurr = {
-    "cz": "Aktuální měna: ",
-    "en": "Actual Currency: "
+//Seznam jazyků, ten první bude vždy brán jako výchozí.
+const langs = ["en","cz"];
+//Texty a překlady na webu, v [0] je jazyk "en", v [1] je jazyk "cz"
+let textTransl = {
+    "zem": ["Country:", "Země:"],
+    "men": ["Currency:", "Měna:"],
+    "kurz_prode": ["We buy for", "Koupíme za:"],
+    "kurz_koup": ["We sell for:", "Prodáme za:"],
+    "aktualni_mena": ["Actual Currency: ", "Aktuální měna: "]
 };
 document.addEventListener("DOMContentLoaded", function all() {
+    console.log("start");
     //Klíč API, ze serveru openexchangerates.org
     const apiKey = '766970c19e8a4cab933b9f562bd4998b'; 
     const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}`;
     //Časovač na 30 minut(1 800 000 milisekund), aby se po 30 minutách spustil celý tento kód a aktualizovaly se data
-    setTimeout(all, 18000000);
+    setTimeout(all, 1800000);
     const parametryUrl = new URLSearchParams(window.location.search);
     //Nastavení hodnot z URL parametrů
-    lang = parametryUrl.get('lang');
+    let lang = parametryUrl.get('lang');
     zaklMena = parametryUrl.get('zaklMena');
     zaklMena = menyseznam[zaklMena];
     i = parametryUrl.get('i');
-    MAXNUM = parametryUrl.get('MAXNUM');
     poradi = parametryUrl.get('poradi') ? parametryUrl.get('poradi').split(',') : [];
     //Nastavení hodnot a1 a a2, což pak následně používám k nastavování url parametrů
-    //Možné zlepšení: Vymyslet to inteligentněji, zkrátit to
-    var a1; //Pro parametr lang
-    var a2; //Pro parametr zaklMena
-    var a3; //Pro parametr i
-    var a4; //Pro parametr MAXNUM
-    var a5 = true; //Pro parametr poradi
+    var Oprava = false; //Parametr, který při true obnoví poškozené url parametry
     //Zjišťování, které url parametry jsou v pořádku a které ne
-    if((lang === "cz")||(lang === "en")){a1 = true;}else{a1 = false;}
-    if(zaklMena in menyseznam){a2 = true;}else{a2 = false;}
-    if((i < 101)&(i > - 1)){a3 = true;}else{a3 = false;}
-    if((MAXNUM < 13)&(MAXNUM > 0)){a4 = true;}else{a4 = false;}
-    //Spravení hodnot i a MAXNUM
-    if(!a3){i = 3;}
-    if(!a4){MAXNUM = 12;}
-
+    if(!(langs.includes(lang))){lang = langs[0]; Oprava = true;}
+    if(!(zaklMena in menyseznam)){zaklMena = "CZK"; Oprava = true;}
+    if((i > 100)|| !(i > -1)|| (i === null)|| (i === "")){i = 3; Oprava = true;}
     //Přípravné nastavení/přidaní/smazání určité hodnot/y v poradi
+    if(poradi.length === 0){
+        poradi[0] = CodesBackup[0];
+        Oprava = true;
+    }
+    MAXNUM = poradi.length;
     let numA = 0;
-    while (numA <= poradi.length - 1){
+    while ((numA <= MAXNUM - 1)){
         if (!(CodesBackup.includes(poradi[numA]))){
             poradi[numA] = CodesBackup[numA];
-            a5 = false;
+            Oprava = true;
         }
         numA = numA + 1;
     }        
-    while (numA <= MAXNUM - 1){
-        poradi[numA] = CodesBackup[numA];
-        numA = numA + 1;
-        a5 = false;
-    }
-    while(poradi.length > MAXNUM){
+    //Prozatimní!--------------------------------------------------------------------------------------<---------------------------------------------
+    while(MAXNUM > 12){
         poradi.pop();
-        a5 = false;
+        MAXNUM = poradi.length;
+        Oprava = true;
     }
-    Codes = poradi;
-
+    Codes = poradi
+    var PovolovacSpusteni = true;
     //Nastavení hodnot, které nejsou v pořádku na základní (pro lang: en, pro zaklMena: CZK)
-    if (!a2 || !a1 || (!a3 || !a4 || !a5) ){
+    if (Oprava === true){
         //Smazání parametrů
         const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
         window.history.replaceState({ path: newUrl }, '', newUrl);
         //Nastavení parametrů, tak aby to, co už bylo nastavené, zůstalo
-        if(!a3||!a4||!a5){
-            window.location.search += '&lang=' + lang +'&zaklMena='+ zaklMena + '&i=' + i + '&MAXNUM=' + MAXNUM +'&poradi=' + Codes;
-        }else if(a1 === a2){
-            window.location.search += '&lang=en&zaklMena=CZK&i='+ i + '&MAXNUM=' + MAXNUM +'&poradi=' + Codes;
-        }else if(!a2){
-            window.location.search += '&lang=' + lang +'&zaklMena=CZK&i='+ i + '&MAXNUM=' + MAXNUM +'&poradi=' + Codes;
-        }else if(!a1){
-            window.location.search += '&lang=en&zaklMena='+ zaklMena + '&i=' + i + '&MAXNUM=' + MAXNUM +'&poradi=' + Codes;
-        }
+        window.location.search += '&lang=' + lang +'&zaklMena='+ zaklMena + '&i=' + i +'&poradi=' + Codes;
+        Oprava = false;
+        PovolovacSpusteni = false;
     }
     //Konec nastatování url parametrů
     //Přizařovač znaku aktuální měny
     const menaVse = menaVs[zaklMena];
     //Zvolení překladů
-    document.getElementById("zem").textContent = translZem[lang];
-    document.getElementById("men").textContent = translMen[lang];
-    document.getElementById("kurz_prode").textContent = translKKo[lang];
-    document.getElementById("kurz_koup").textContent = translKPr[lang];
+    let langNUM;
+    if(lang === langs[1]){
+        langNUM = 1;
+    }else if(lang === langs[0]){
+        langNUM = 0;
+    }
+    const divTexty = ["zem", "men", "kurz_prode", "kurz_koup"];
+    let translNUM = 0;
+    while(translNUM < divTexty.length){
+        document.getElementById(divTexty[translNUM]).textContent = textTransl[divTexty[translNUM]][langNUM];
+        translNUM = translNUM + 1;
+    }
+   //Určovač, co bude se zobrazí za vlajku, když na ni kliknete
+    document.querySelector('.jazyk').innerHTML = `<img id="lang_img" src="${lang}.png" alt="${lang}">`;
     //Funkce, která mi volí jazyky (čeština= item1, angličtina=item2)
     function languager(lang, item1, item2){
         var ccc;
@@ -124,65 +105,64 @@ document.addEventListener("DOMContentLoaded", function all() {
     //Nastavování jazyků 
     const menaStat = languager(lang, menaStatCZ,menaStatEN);
     const menaJmeno = languager(lang, menaJmenoCZ,menaJmenoEN);
-    //Připojení na https://openexchangerates.org/api/latest.json?app_id=${apiKey}
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            //Tyto odkazy jsou základní třídy bez čísel, vždycky se z nim nakonec připojí to číslo a vznikne třída, kterou mám zapsanou v divech
-            var odkazkupr ='.kurz_prodej';
-            var odkazkuko ='.kurz_koupe';
-            var odkazze = '.zeme';
-            var odkazme ='.mena';
-            var odkazvl = '.zeme_vlajka';
-            let number = 0;
-            //czk je základní(aktuální) hodnota měny, podle které se všechno řídí
-            const czk = data.rates[zaklMena]; 
-            document.querySelector('.aktualni_mena').textContent = `${actCurr[lang]}${menaJmeno[zaklMena]} (${zaklMena})`;
-            //Určovač, co bude se zobrazí za vlajku, když na ni kliknete
-            if(lang === "cz"){
-                document.querySelector('.jazyk').innerHTML = `<img id="lang_img" src="en.png" alt="en">`;
-            }else if(lang === "en"){
-                document.querySelector('.jazyk').innerHTML = `<img class="zeme_vlajky" src="cz.png" alt="cz">`;
-            }
-            //Nastavení hodnoty na prodej
-            function exchangeRat(currency){
-                const exchange = czk / data.rates[currency];
-                const aaa = `${exchange.toFixed(i)} ${menaVse}`;
-                return aaa;
-            }
-            //Nastavení hodnoty na koupi
-            function buyRat(currency){
-                const buy = czk / data.rates[currency] * (1 - zdrazeni);
-                const bbb = `${buy.toFixed(i)} ${menaVse}`;
-                return bbb;
-            }
-            //Funkce která jednotlivým divům přiřazuje to co k nim má zrovna patřit (MAXNUM je zde maxiální číslo u jednotlivých divů )
-            while (number < MAXNUM){
-                currencyCode = Codes[number];
-                number = number + 1;
+    //Připojení na https://openexchangerates.org/api/latest.json?app_id=${apiKey} pouze při správných parametrech
+    if(PovolovacSpusteni){
+        console.log("start--");
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                //Tyto odkazy jsou základní třídy bez čísel, vždycky se z nim nakonec připojí to číslo a vznikne třída, kterou mám zapsanou v divech
+                var odkazkupr ='.kurz_prodej';
+                var odkazkuko ='.kurz_koupe';
+                var odkazze = '.zeme';
+                var odkazme ='.mena';
+                var odkazvl = '.zeme_vlajka';
+                let number = 0;
+                //czk je základní(aktuální) hodnota měny, podle které se všechno řídí
+                const czk = data.rates[zaklMena]; 
+                document.querySelector('.aktualni_mena').textContent = `${textTransl["aktualni_mena"][langNUM]}${menaJmeno[zaklMena]} (${zaklMena})`;
 
-                var cislo = String (number);
-                var odkaz1 = odkazkupr + cislo;
-                var odkaz2 = odkazkuko + cislo;
-                var odkaz3 = odkazze + cislo;
-                var odkaz4 = odkazme + cislo;
-                var odkaz5 = odkazvl + cislo;
-                //Žluté označení v případě, že se zrovna dělá řádek s měnou, která je zároveň nastavená jako základní(aktuální)
-                if(currencyCode === zaklMena){
-                    document.querySelector(odkaz1).innerHTML  = `<div class="actual">${exchangeRat(currencyCode)}</div>`;
-                    document.querySelector(odkaz2).innerHTML  = `<div class="actual">${buyRat(currencyCode)}</div>`;
-                    document.querySelector(odkaz3).innerHTML = `<div class="actual">${menaStat[currencyCode]} (${currencyCode})</div>`;
-                    document.querySelector(odkaz4).innerHTML  = `<div class="actual">${menaJmeno[currencyCode]} (${menaVs[currencyCode]})</div>`;
-                }else{
-                    document.querySelector(odkaz1).textContent  = exchangeRat(currencyCode);
-                    document.querySelector(odkaz2).textContent  = buyRat(currencyCode);
-                    document.querySelector(odkaz3).textContent = `${menaStat[currencyCode]} (${currencyCode})`;
-                    document.querySelector(odkaz4).textContent  = `${menaJmeno[currencyCode]}(${menaVs[currencyCode]})`;
+                //Nastavení hodnoty na prodej
+                function exchangeRat(currency){
+                    const exchange = czk / data.rates[currency];
+                    const aaa = `${exchange.toFixed(i)} ${menaVse}`;
+                    return aaa;
                 }
-                document.querySelector(odkaz5).innerHTML = `<img class="zeme_vlajky" src="https://flagcdn.com/w40/${menaVlajky[currencyCode]}.png" alt="${menaStat[currencyCode]}">`;
-            }
-        })
-        .catch(error => console.error('Chyba:', error));
+                //Nastavení hodnoty na koupi
+                function buyRat(currency){
+                    const buy = czk / data.rates[currency] * (1 - zdrazeni);
+                    const bbb = `${buy.toFixed(i)} ${menaVse}`;
+                    return bbb;
+                }
+                //Funkce která jednotlivým divům přiřazuje to co k nim má zrovna patřit (MAXNUM je zde maxiální číslo u jednotlivých divů )
+                while (number < MAXNUM){
+                    currencyCode = Codes[number];
+                    number = number + 1;
+    
+                    var cislo = String (number);
+                    var odkaz1 = odkazkupr + cislo;
+                    var odkaz2 = odkazkuko + cislo;
+                    var odkaz3 = odkazze + cislo;
+                    var odkaz4 = odkazme + cislo;
+                    var odkaz5 = odkazvl + cislo;
+                    //Žluté označení v případě, že se zrovna dělá řádek s měnou, která je zároveň nastavená jako základní(aktuální)
+                    if(currencyCode === zaklMena){
+                        document.querySelector(odkaz1).innerHTML  = `<div class="actual">${exchangeRat(currencyCode)}</div>`;
+                        document.querySelector(odkaz2).innerHTML  = `<div class="actual">${buyRat(currencyCode)}</div>`;
+                        document.querySelector(odkaz3).innerHTML = `<div class="actual">${menaStat[currencyCode]} (${currencyCode})</div>`;
+                        document.querySelector(odkaz4).innerHTML  = `<div class="actual">${menaJmeno[currencyCode]} (${menaVs[currencyCode]})</div>`;
+                    }else{
+                        document.querySelector(odkaz1).textContent  = exchangeRat(currencyCode);
+                        document.querySelector(odkaz2).textContent  = buyRat(currencyCode);
+                        document.querySelector(odkaz3).textContent = `${menaStat[currencyCode]} (${currencyCode})`;
+                        document.querySelector(odkaz4).textContent  = `${menaJmeno[currencyCode]}(${menaVs[currencyCode]})`;
+                    }
+                    document.querySelector(odkaz5).innerHTML = `<img class="zeme_vlajky" src="https://flagcdn.com/w40/${menaVlajky[currencyCode]}.png" alt="${menaStat[currencyCode]}">`;
+                }
+            })
+            .catch(error => console.error('Chyba:', error));
+    }
+    PovolovacSpusteni = true;
 });
 //Funkce, kterou používám při kliknutí na vlajku, která následně změní jazyk a obrázek v url parametrech
 function changeLanguage(){
@@ -190,17 +170,22 @@ function changeLanguage(){
     lang = parametryUrl.get('lang');
     zaklMena = parametryUrl.get('zaklMena');
     i = parametryUrl.get('i');
-    MAXNUM = parametryUrl.get('MAXNUM');
     poradi = parametryUrl.get('poradi') ? parametryUrl.get('poradi').split(',') : [];
+    let searchNUM = 0;
+    while (!(langs[searchNUM] === lang)){
+        searchNUM = searchNUM + 1;
+    }
+    if(searchNUM === langs.length - 1){
+        searchNUM = 0;
+    }else if(searchNUM > langs.length - 1){
+        searchNUM = 0;
+    }else{
+        searchNUM = searchNUM + 1;
+    }
+    lang = langs[searchNUM];
     const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
     window.history.replaceState({ path: newUrl }, '', newUrl);
-    if(lang === "cz"){
-        window.location.search += '&lang=en&zaklMena='+ zaklMena +'&i=' + i + '&MAXNUM=' + MAXNUM +'&poradi=' + poradi;;
-        document.querySelector('.jazyk').innerHTML = `<img id="lang_img" src="en.png" alt="en">`;
-    }else if(lang === "en"){
-        window.location.search += '&lang=cz&zaklMena='+ zaklMena +'&i=' + i+ '&MAXNUM=' + MAXNUM +'&poradi=' + poradi;;
-        document.querySelector('.jazyk').innerHTML = `<img class="zeme_vlajky" src="cz.png" alt="cz">`;
-    }
+    window.location.search += '&lang=' + lang +'&zaklMena='+ zaklMena + '&i=' + i +'&poradi=' + Codes;
 }
 //Funkce, kterou používám při kliknutí na aktuální měnu, která následně změní měnu v url parametrech
 function changeMena(){
@@ -208,7 +193,6 @@ function changeMena(){
     lang = parametryUrl.get('lang');
     zaklMena = parametryUrl.get('zaklMena');
     i = parametryUrl.get('i');
-    MAXNUM = parametryUrl.get('MAXNUM');
     poradi = parametryUrl.get('poradi') ? parametryUrl.get('poradi').split(',') : [];
     const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
     window.history.replaceState({ path: newUrl }, '', newUrl);
@@ -224,7 +208,7 @@ function changeMena(){
         numberHledaci = numberHledaci + 1;
     }
     zaklMena = CodesBackup[numberHledaci];
-    window.location.search += '&lang=' + lang + '&zaklMena='+ zaklMena +'&i=' + i + '&MAXNUM=' + MAXNUM +'&poradi=' + poradi;
+    window.location.search += '&lang=' + lang +'&zaklMena='+ zaklMena + '&i=' + i +'&poradi=' + Codes;
 }
 //Funkce, kterou používám při kliknutí na určitý název státu, kde se používá měna, která následně změní stát v url paramatrech, posune se to o stát dál
 function changeDivMena(numB){
@@ -232,9 +216,7 @@ function changeDivMena(numB){
     lang = parametryUrl.get('lang');
     zaklMena = parametryUrl.get('zaklMena');
     i = parametryUrl.get('i');
-    MAXNUM = parametryUrl.get('MAXNUM');
     poradi = parametryUrl.get('poradi') ? parametryUrl.get('poradi').split(',') : [];
-
     let numberHledaci = 0;
     while (!(CodesBackup[numberHledaci] === poradi[numB-1])){
         numberHledaci = numberHledaci + 1;
@@ -247,11 +229,45 @@ function changeDivMena(numB){
         numberHledaci = numberHledaci + 1;
     }
     poradi[numB-1] = CodesBackup[numberHledaci];
-
-
     Codes = poradi;
     const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
-    
     window.history.replaceState({ path: newUrl }, '', newUrl);
-    window.location.search += '&lang=' + lang + '&zaklMena='+ zaklMena +'&i=' + i+ '&MAXNUM=' + MAXNUM +'&poradi=' + poradi;;
+    window.location.search += '&lang=' + lang +'&zaklMena='+ zaklMena + '&i=' + i +'&poradi=' + Codes;
+}
+//Funkce přidávající další měnu
+function pridaniRadkuMeny(){
+    const parametryUrl = new URLSearchParams(window.location.search);
+    lang = parametryUrl.get('lang');
+    zaklMena = parametryUrl.get('zaklMena');
+    i = parametryUrl.get('i');
+    poradi = parametryUrl.get('poradi') ? parametryUrl.get('poradi').split(',') : [];
+    poradi.push(CodesBackup[0]);
+    MAXNUM = poradi.length;   
+    //Prozatimní!--------------------------------------------------------------------------------------<---------------------------------------------
+    if(MAXNUM > 12){
+        poradi.pop();
+        MAXNUM = poradi.length;
+    }
+    Codes = poradi;
+    const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+    window.history.replaceState({ path: newUrl }, '', newUrl);
+    window.location.search += '&lang=' + lang +'&zaklMena='+ zaklMena + '&i=' + i +'&poradi=' + Codes;
+}
+//Funkce odebírající měnu
+function odstraneniRadkuMeny(){
+    const parametryUrl = new URLSearchParams(window.location.search);
+    lang = parametryUrl.get('lang');
+    zaklMena = parametryUrl.get('zaklMena');
+    i = parametryUrl.get('i');
+    poradi = parametryUrl.get('poradi') ? parametryUrl.get('poradi').split(',') : [];
+    poradi.pop();
+    MAXNUM = poradi.length;   
+    //Prozatimní!--------------------------------------------------------------------------------------<---------------------------------------------
+    if(poradi.length === 0){
+        poradi[0] = CodesBackup[0];
+    }
+    Codes = poradi;
+    const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+    window.history.replaceState({ path: newUrl }, '', newUrl);
+    window.location.search += '&lang=' + lang +'&zaklMena='+ zaklMena + '&i=' + i +'&poradi=' + Codes;
 }
